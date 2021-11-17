9819b5ce2c48d9eff4071ac8d9d33a0cf9689c2f
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-11-18 00:18:27 +0600

    when and how we apply bind method

--- callApply.js
+++ callApply.js
@@ -6,10 +6,22 @@ const normalPerson = {
         console.log(this.firstName, this.lastName);
     },
     chargeBill: function(amount){
+        console.log(this);
         this.salary = this.salary - amount;
         return this.salary;
     }
 }
 
-normalPerson.chargeBill(1500);
-console.log(normalPerson.salary);
+const heroPerson = {
+    firstName : 'Hero',
+    lastName: 'Alom',
+    salary: 25000,
+}
+
+// normalPerson.chargeBill(1500);
+// console.log(normalPerson.salary);
+// normalPerson.chargeBill();
+const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+heroChargeBill(2000); 
+heroChargeBill(3000); 
+console.log(heroPerson.salary);
