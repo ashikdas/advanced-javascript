afb0cacb612b826d9d44138a3691890c4f5e26d1
Ashik Das <43861636+ashikdas@users.noreply.github.com>
2021-11-18 12:42:23 +0600

    uses of call method

--- callApply.js
+++ callApply.js
@@ -5,9 +5,9 @@ const normalPerson = {
     getFullName: function(){
         console.log(this.firstName, this.lastName);
     },
-    chargeBill: function(amount){
+    chargeBill: function(amount, tips, tax){
         console.log(this);
-        this.salary = this.salary - amount;
+        this.salary = this.salary - amount - tips - tax;
         return this.salary;
     }
 }
@@ -21,7 +21,11 @@ const heroPerson = {
 // normalPerson.chargeBill(1500);
 // console.log(normalPerson.salary);
 // normalPerson.chargeBill();
-const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
-heroChargeBill(2000); 
-heroChargeBill(3000); 
+
+// const heroChargeBill = normalPerson.chargeBill.bind(heroPerson);
+// heroChargeBill(2000); 
+// heroChargeBill(3000); 
+// console.log(heroPerson.salary);
+
+normalPerson.chargeBill.call(heroPerson, 900, 199, 14);
 console.log(heroPerson.salary);
